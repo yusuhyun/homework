c85eeaa4889aaaf0b1cb04dadb84a820a00915b6
yusuhyun <dbtngus33@naver.com>
2022-09-02 00:14:54 +0900

    throws 오류 수정

--- src/homework03_2/run/Run.java
+++ src/homework03_2/run/Run.java
@@ -1,17 +1,13 @@
 package homework03_2.run;
 
+import java.util.Scanner;
+
 import homework03_2.view.BookMenu;
 
 public class Run {
+	static BookMenu bm = new BookMenu();
 	public static void main(String[] args) {
-		try {
 			new BookMenu().mainMenu();
-		} catch(NumberFormatException e) {
-			new BookMenu().displayError("숫자를 입력해 주세요.");
-		} catch(Exception e) {
-			e.printStackTrace();
-		} finally {
-			new BookMenu().mainMenu();
-		}
+		
 	}
 }
--- src/homework03_2/view/BookMenu.java
+++ src/homework03_2/view/BookMenu.java
@@ -3,34 +3,44 @@ package homework03_2.view;
 import java.util.Scanner;
 
 import homework03_2.controller.BookController;
+import homework03_2.run.Run;
 
 
 public class BookMenu {
-	Scanner sc = new Scanner(System.in);
+	public Scanner sc = new Scanner(System.in);
 	
 	public void mainMenu() throws NumberFormatException {
 		BookController bc = new BookController();
 		int sel = 0;
-		do {
-			System.out.println("\n === 도서 관리 프로그램__StreamVer3 === \n");
-			System.out.println("1. 전체 도서 조회");
-			System.out.println("2. 도서 검색");
-			System.out.println("3. 신규 도서 등록");
-			System.out.println("4. 도서 정보 수정");
-			System.out.println("5. 도서 정보 삭제");
-			
-			sel = Integer.parseInt(sc.nextLine());
-			
-			switch(sel) {
-			case 1: bc.selectAll(); break; 
-			case 2: bc.selectBook(); break;
-			case 3: bc.insertBook(); break;
-			case 4: bc.updateBook(); break;
-			case 5: bc.deleteBook(); break;
-			default: displayError("잘못된 번호입니다. 다시 입력해 주세요.");
-			}
-			
-		} while(sel !=0);
+		try {
+			do {
+				System.out.println("\n === 도서 관리 프로그램__StreamVer3 === \n");
+				System.out.println("1. 전체 도서 조회");
+				System.out.println("2. 도서 검색");
+				System.out.println("3. 신규 도서 등록");
+				System.out.println("4. 도서 정보 수정");
+				System.out.println("5. 도서 정보 삭제");
+				
+				sel = Integer.parseInt(sc.nextLine());
+				
+				switch(sel) {
+				case 1: bc.selectAll(); break; 
+				case 2: bc.selectBook(); break;
+				case 3: bc.insertBook(); break;
+				case 4: bc.updateBook(); break;
+				case 5: bc.deleteBook(); break;
+				default: displayError("잘못된 번호입니다. 다시 입력해 주세요.");
+				}
+				
+			} while(sel !=0);
+		} catch(NumberFormatException e){
+			displayError("숫자를 입력해 주세요.");
+		} catch(Exception e) {
+			e.printStackTrace();
+		} finally {
+			mainMenu();
+		}
+		
 		
 	}
 	
